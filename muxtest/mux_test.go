43e4e6e8019050530de51f6551ecd6b91b0b5672
open_china <953761580@qq.com>
2019-03-10 12:38:51 +0800

    添加测试内容

--- muxtest/mux_test.go
+++ muxtest/mux_test.go
@@ -1,1 +1,29 @@
 package muxtest
+
+import (
+	"fmt"
+	"github.com/itzujun/go-radixtree"
+	"testing"
+)
+
+func TestMux(t *testing.T) {
+
+	tree := goradixtree.NewRadixTree()
+
+	tree.Insert(1, "/hello", func(name string) string {
+			return "你好：" + name
+		}).Insert(1, "/world", func(s string) string {
+			return "欢迎来到：" + s
+		})
+
+	node := tree.FindNode("/hello")
+	if node != nil {
+		fmt.Println(node.Handler("马云"))
+	}
+
+	node = tree.FindNode("/world")
+	if node != nil {
+		fmt.Println(node.Handler("北京"))
+	}
+
+}
